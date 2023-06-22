63f7da19e99d08b76d01431b907b2c1b4c86801e
nom0531 <norrrrrra0504@gmail.com>
2023-06-22 09:23:05 +0900

    衝突判定用のコリジョン属性を指定するコードを追加

--- Mining/Game/Stage/BackGround.cpp
+++ Mining/Game/Stage/BackGround.cpp
@@ -21,6 +21,7 @@ bool BackGround::Start()
 	m_modelRender.Update();
 
 	m_physicsStaticObjct.CreateFromModel(m_modelRender.GetModel(), m_modelRender.GetModel().GetWorldMatrix());
+	m_physicsStaticObjct.GetbtCollisionObject()->setUserIndex(enCollisionAttr_Wall);
 
 	return true;
 }
